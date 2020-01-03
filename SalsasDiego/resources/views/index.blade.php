8726a19edb8feb4b6948208fa0dfc9d16d808142
Francisco Garcia Hernandez <49650537+FranciscoGh12@users.noreply.github.com>
2020-01-03 14:38:19 -0600

    Index 15%
    Co-Authored-By: Francisco Garcia Hernandez <franciscogh12@users.noreply.github.com>

--- SalsasDiego/resources/views/index.blade.php
+++ SalsasDiego/resources/views/index.blade.php
@@ -58,7 +58,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
             <i><img style="height:100px; width:100px;" src="{{asset('frontend/images/circlelogosmall.png')}}" alt=""></i>
-            <a class="navbar-brand" href="index.html">Salsas Don Diego</a>
+            <a class="navbar-brand" href="#">Salsas Don Diego</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
                 aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="oi oi-menu"></span> Menu
@@ -66,7 +66,7 @@
 
             <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="index.html" class="nav-link">Inicio</a></li>
+                    <li class="nav-item active"><a href="#" class="nav-link">Inicio</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Nosotros</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Recetas</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Contacto</a></li>
@@ -161,63 +161,14 @@
         </div>
     </section>
 
-    <section class="ftco-section ftco-category ftco-no-pt">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="row">
-                        <div class="col-md-6 order-md-last align-items-stretch d-flex">
-                            <div class="category-wrap-2 ftco-animate img align-self-stretch d-flex"
-                                style="background-image: url(images/category.jpg);">
-                                <div class="text text-center">
-                                    <h2>Vegetables</h2>
-                                    <p>Protect the health of every home</p>
-                                    <p><a href="#" class="btn btn-primary">Shop now</a></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end"
-                                style="background-image: url(images/category-1.jpg);">
-                                <div class="text px-3 py-1">
-                                    <h2 class="mb-0"><a href="#">Fruits</a></h2>
-                                </div>
-                            </div>
-                            <div class="category-wrap ftco-animate img d-flex align-items-end"
-                                style="background-image: url(images/category-2.jpg);">
-                                <div class="text px-3 py-1">
-                                    <h2 class="mb-0"><a href="#">Vegetables</a></h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end"
-                        style="background-image: url(images/category-3.jpg);">
-                        <div class="text px-3 py-1">
-                            <h2 class="mb-0"><a href="#">Juices</a></h2>
-                        </div>
-                    </div>
-                    <div class="category-wrap ftco-animate img d-flex align-items-end"
-                        style="background-image: url(images/category-4.jpg);">
-                        <div class="text px-3 py-1">
-                            <h2 class="mb-0"><a href="#">Dried</a></h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
     <section class="ftco-section">
         <div class="container">
             <div class="row justify-content-center mb-3 pb-3">
                 <div class="col-md-12 heading-section text-center ftco-animate">
-                    <span class="subheading">Featured Products</span>
-                    <h2 class="mb-4">Our Products</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                    <span class="subheading">Catalogo de Productos</span>
+                    <h2 class="mb-4">Nuestros Productoss</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, laboriosam.</p>
                 </div>
             </div>
         </div>
@@ -493,124 +444,30 @@
         </div>
     </section>
 
-    <section class="ftco-section testimony-section">
-        <div class="container">
-            <div class="row justify-content-center mb-5 pb-3">
-                <div class="col-md-7 heading-section ftco-animate text-center">
-                    <span class="subheading">Testimony</span>
-                    <h2 class="mb-4">Our satisfied customer says</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
-                        live the blind texts. Separated they live in</p>
-                </div>
-            </div>
-            <div class="row ftco-animate">
-                <div class="col-md-12">
-                    <div class="carousel-testimony owl-carousel">
-                        <div class="item">
-                            <div class="testimony-wrap p-4 pb-5">
-                                <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="icon-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text text-center">
-                                    <p class="mb-5 pl-4 line">Far far away, behind the word mountains, far from the
-                                        countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="name">Garreth Smith</p>
-                                    <span class="position">Marketing Manager</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap p-4 pb-5">
-                                <div class="user-img mb-5" style="background-image: url(images/person_2.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="icon-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text text-center">
-                                    <p class="mb-5 pl-4 line">Far far away, behind the word mountains, far from the
-                                        countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="name">Garreth Smith</p>
-                                    <span class="position">Interface Designer</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap p-4 pb-5">
-                                <div class="user-img mb-5" style="background-image: url(images/person_3.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="icon-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text text-center">
-                                    <p class="mb-5 pl-4 line">Far far away, behind the word mountains, far from the
-                                        countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="name">Garreth Smith</p>
-                                    <span class="position">UI Designer</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap p-4 pb-5">
-                                <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="icon-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text text-center">
-                                    <p class="mb-5 pl-4 line">Far far away, behind the word mountains, far from the
-                                        countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="name">Garreth Smith</p>
-                                    <span class="position">Web Developer</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap p-4 pb-5">
-                                <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="icon-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text text-center">
-                                    <p class="mb-5 pl-4 line">Far far away, behind the word mountains, far from the
-                                        countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="name">Garreth Smith</p>
-                                    <span class="position">System Analyst</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
     <hr>
 
     <section class="ftco-section ftco-partner">
         <div class="container">
             <div class="row">
                 <div class="col-sm ftco-animate">
-                    <a href="#" class="partner"><img src="images/partner-1.png" class="img-fluid"
-                            alt="Colorlib Template"></a>
+                    <a href="#" class="partner"><img src="{{asset('frontend/images/codet.png')}}" class="img-fluid"
+                            alt="Code-T"></a>
                 </div>
                 <div class="col-sm ftco-animate">
-                    <a href="#" class="partner"><img src="images/partner-2.png" class="img-fluid"
-                            alt="Colorlib Template"></a>
+                    <a href="#" class="partner"><img src="{{asset('frontend/images/codet.png')}}" class="img-fluid"
+                            alt="Code-T"></a>
                 </div>
                 <div class="col-sm ftco-animate">
-                    <a href="#" class="partner"><img src="images/partner-3.png" class="img-fluid"
-                            alt="Colorlib Template"></a>
+                    <a href="#" class="partner"><img src="{{asset('frontend/images/codet.png')}}" class="img-fluid"
+                            alt="Code-T"></a>
                 </div>
                 <div class="col-sm ftco-animate">
-                    <a href="#" class="partner"><img src="images/partner-4.png" class="img-fluid"
-                            alt="Colorlib Template"></a>
+                    <a href="#" class="partner"><img src="{{asset('frontend/images/codet.png')}}" class="img-fluid"
+                            alt="Code-T"></a>
                 </div>
                 <div class="col-sm ftco-animate">
-                    <a href="#" class="partner"><img src="images/partner-5.png" class="img-fluid"
-                            alt="Colorlib Template"></a>
+                    <a href="#" class="partner"><img src="{{asset('frontend/images/codet.png')}}" class="img-fluid"
+                            alt="Code-T"></a>
                 </div>
             </div>
         </div>
